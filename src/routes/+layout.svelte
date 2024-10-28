dd4ffc83e82f825a16d7f12fd54861fe9fdd1c6e
kigarde <kigardetom2001@gmail.com>
2024-10-28 11:36:04 +0300

    add logo, images and theme colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,33 @@
 <script>
-    import '../app.css'
-    import cart from '$lib/Cart.svelte'
+	import '../app.css';
+	import cart from '$lib/Cart.svelte';
 
-    let {children} = $props()
+	let { children } = $props();
 </script>
 
-<header class="flex justify-between p-4 bg-gray-800 text-white">
-    <h1 class="text-2xl font-bold">KenyaLaw E-comm</h1>
-    <nav class="flex items-center">
-        <ul class="flex">
-            <li class="mx-4 hover:text-yellow-400 transition-colors"><a href="/">Home</a></li>
-            <li class="mx-4 hover:text-yellow-400 transition-colors"><a href="/products">Products</a></li>
-            <li class="mx-4 hover:text-yellow-400 transition-colors"><a href="/about">About</a></li>
-        </ul>
-    </nav>
-    <div>
-        <button class="bg-yellow-500 rounded hover:bg-amber-500 transition-colors p-2" onclick={()=> cart.cartopen = !cart.cartopen}> Cart: {cart.cartStats.quantity}</button>
-    </div>
+<div class="m-5">
+	<img src="/kLawLogo.png" alt="logo" class="h-10 md:h-12 lg:h-20" />
+</div>
+
+<header class="flex justify-between bg-fadeblack p-2 text-white">
+	<nav class="flex items-center">
+		<ul class="flex">
+			<li class="mx-4 transition-colors hover:text-secondary"><a href="/">Home</a></li>
+			<li class="mx-4 transition-colors hover:text-secondary"><a href="/products">Products</a></li>
+			<li class="mx-4 transition-colors hover:text-secondary"><a href="/about">About</a></li>
+		</ul>
+	</nav>
+	<div>
+		<button
+			class="rounded bg-secondary p-2 transition-colors hover:bg-secondary"
+			onclick={() => (cart.cartopen = !cart.cartopen)}
+		>
+			Cart: {cart.cartStats.quantity}</button
+		>
+	</div>
 </header>
 
 <div>
-    {@render children?.()}
+	{@render children?.()}
 </div>
 <footer class=""></footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,14 @@
 <!-- Cart -->
 {#if cart.cartopen}
 	<div
-		class="absolute right-0 top-15 bg-white rounded-lg shadow-lg"
+		class="absolute right-0 top-28 rounded-md bg-white shadow-lg"
 		in:fade={{ duration: 200 }}
 		out:fade={{ duration: 200 }}
 	>
 		<div class="relative p-4">
 			<h2 class="text-lg font-bold">Your cart</h2>
 			<button
-				class="absolute right-2 top-3 rounded-full p-1 hover:bg-gray-100 font-bold"
+				class="absolute right-2 top-3 rounded-full p-1 font-bold hover:bg-gray-100"
 				aria-label="close cart"
 				onclick={() => (cart.cartopen = false)}>×</button
 			>
@@ -25,29 +25,34 @@
 				{#each cart.cartItems as item, i}
 					<CartItem {item} />
 				{/each}
+				<div class="mt-3">
+					<p class="font-semibold">Total: {cart.cartStats.total.toFixed(2)}</p>
+				</div>
+
+				<button
+					class="rounded-md border border-gray-300 px-2 py-1 transition-colors hover:bg-primary hover:text-white"
+				>
+					Proceed to checkout
+				</button>
 			{:else}
 				<div>
 					<i class="text-sm">Your cart is empty</i>
 				</div>
 			{/if}
-
-			<div class="mt-3">
-				<p class="font-semibold">Total: {cart.cartStats.total.toFixed(2)}</p>
-			</div>
 		</div>
 	</div>
 {/if}
-<main class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+<main class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 	{#each data.products as product, i}
-		<div class=" overflow-hidden rounded-xl shadow-lg p-4 m-3">
-			<img src={product.thumbnail} alt={product.title} class="object-cover h-36 m-auto" />
+		<div class=" m-3 overflow-hidden rounded-md border border-gray-300 p-4">
+			<img src={product.thumbnail} alt={product.title} class="m-auto h-36 object-cover" />
 			<div class="p-4">
-				<h4 class="text-xl text-gray-800">{product.title}</h4>
-				<p class="truncate m-1 text-sm">{product.description}</p>
+				<h4 class="text-xl text-fadeblack">{product.title}</h4>
+				<p class="m-1 truncate text-sm">{product.description}</p>
 				<div class="flex items-center justify-between">
-					<span class="font-semibold">Ksh {product.price}</span>
+					<span class="font-semibold">KES {product.price}</span>
 					<button
-						class="bg-red-900 text-white py-1 px-4 rounded-lg hover:bg-red-950 duration-300"
+						class="rounded-lg bg-primary px-4 py-1 text-white duration-300 hover:bg-primary"
 						onclick={() => cart.cartItems.push({ id: crypto.randomUUID(), quantity: 1, product })}
 						>Add to cart</button
 					>
