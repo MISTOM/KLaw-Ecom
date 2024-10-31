2ca9486602bb1b9933f7ad3b7b16c23c2b6f33c9
kigarde <kigardetom2001@gmail.com>
2024-10-31 11:40:55 +0300

    add jwt auth, saving tokens in http only cookies

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,20 +1,27 @@
 import prisma from '$lib/server/prisma.js';
 import auth from '$lib/server/auth.js';
-import { type Actions, error, fail } from '@sveltejs/kit';
-import { goto } from '$app/navigation';
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types'
+
+export const load = (async ({ locals }) => {
+	console.log('this user is here', locals);
+	if (locals.user) {
+		return redirect(301, '/admin/product');
+	}
+}) satisfies PageServerLoad
 
 export const actions = {
-	default: async ({ request, locals }) => {
+	default: async ({ request, cookies }) => {
 		const formData = await request.formData();
 		console.log(formData);
 		const email = formData.get('email');
 		const password = formData.get('password');
 
 		if (!email || !password) {
-			return {
+			return fail(400, {
 				data: { email },
 				errors: 'Username and password required'
-			};
+			});
 		}
 
 		let user;
@@ -23,28 +30,34 @@ export const actions = {
 				where: { email: email.toString() }
 			});
 			if (!user) {
-				return {
+				return fail(400, {
 					data: { email },
 					errors: 'Invalid emmail or password'
-				};
+				});
 			}
 		} catch (e) {
 			console.log(e);
-			return {
-				data: email,
-				errors: e
-			};
+			return fail(500, {
+				data: { email },
+				errors: 'Internal Server Error'
+			});
 		}
 
-		const isvalidPassword = await auth.compare(password, user.password);
+		const isvalidPassword = await auth.compare(password.toString(), user.password);
 		if (!isvalidPassword) {
-			return {
+			return fail(401, {
 				data: { email },
 				errors: 'Invalid email or passsword'
-			};
+			});
 		}
 		const token = auth.sign(user);
 		const refreshToken = await auth.generateRefreshToken(user);
-		console.log(token, refreshToken);
+
+		//TODO - Set this globally
+		const maxAge = 60 * 60 * 24 * 7; // 1 week
+		cookies.set('token', token, { httpOnly: true, secure: true, path: '/', maxAge });
+		cookies.set('refreshToken', refreshToken, { httpOnly: true, secure: true, path: '/', maxAge });
+
+		redirect(301, '/admin/product')
 	}
 } satisfies Actions;
